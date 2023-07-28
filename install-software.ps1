6783d50a0b15f08a31033f4333cf6a30a721d37d
sivag251993 <58213214+sivag251993@users.noreply.github.com>
2023-07-28 11:14:49 +0530

    Update install-software.ps1

--- install-software.ps1
+++ install-software.ps1
@@ -2,7 +2,7 @@
 Write-Host "Installing software..."
 
 # Example: Install Chrome
-$chromeInstallerUrl = "https://sivastorageavd001.blob.core.windows.net/sofftware/ChromeStandaloneSetup64.exe?sp=r&st=2023-07-26T06:42:46Z&se=2023-08-26T14:42:46Z&spr=https&sv=2022-11-02&sr=b&sig=2AhjkvHBc5fYfMBXFlBjG5g7UTYN5A55f9BIi6BVdio%3D"
+$chromeInstallerUrl = "https://avdpipe.blob.core.windows.net/software/ChromeStandaloneSetup64.exe?sp=r&st=2023-07-28T04:48:41Z&se=2023-08-28T12:48:41Z&spr=https&sv=2022-11-02&sr=b&sig=2zLo%2B4vEgqWOYT3%2FV%2BRPP43IdRcng1Z8At7ZrGPaSMM%3D"
 # $chromeInstallerPath = "C:\Temp\chrome_installer.exe"
 
 # # Download Chrome installer
