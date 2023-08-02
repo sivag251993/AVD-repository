7dfe7abc67781732d20c54451256d8327436ac99
sivag251993 <58213214+sivag251993@users.noreply.github.com>
2023-08-02 11:44:12 +0530

    Update 2_Available_StopVms_with_drain_on.ps1

--- 2_Available_StopVms_with_drain_on.ps1
+++ 2_Available_StopVms_with_drain_on.ps1
@@ -3,11 +3,11 @@
  
  # Run after 2 hrs
  
-$hostPoolRg="avd-rg";
+$hostPoolRg="SivaG-RG";
 $hostPoolName="hostpool-avs";
 
 
-$runningSessionHosts = Get-AzWvdSessionHost -ErrorAction Stop -HostPoolName $hostPoolName -ResourceGroupName $hostPoolRg | Where-Object { $_.Status -eq "Available" } | Where-Object { $_.AllowNewSession -eq $false } | Where-Object {$_.Name -like '*win11-avs*'};
+$runningSessionHosts = Get-AzWvdSessionHost -ErrorAction Stop -HostPoolName $hostPoolName -ResourceGroupName $hostPoolRg | Where-Object { $_.Status -eq "Available" } | Where-Object { $_.AllowNewSession -eq $false } | Where-Object {$_.Name -like '*win11-avs-2*'};
 
 
 # Stop all vms which are in drain mode
